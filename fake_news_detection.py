53d82bf5e748cd70b0e6c1c6d78f27ffd209075b
unknown <anuradhac12@gmail.com>
2021-01-13 22:09:35 -0800

    model PassiveAggressiveClassifier

--- fake_news_detection.py
+++ fake_news_detection.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 
@@ -16,19 +16,19 @@ y = news['label']
 #Splitting the data into train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
+#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies PassiveAggressiveClassifier model
 pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
-                    ('nbmodel', MultinomialNB())])
+                    ('pac', PassiveAggressiveClassifier())])
 
 #Training our data
 pipeline.fit(X_train, y_train)
 
 #Predicting the label for the test data
-pred = pipeline.predict(X_test)
+ypred = pipeline.predict(X_test)
 
 #Checking the performance of our model
-print(classification_report(y_test, pred))
-print(confusion_matrix(y_test, pred))
+print(classification_report(y_test, ypred))
+print(confusion_matrix(y_test, ypred))
 
 #Serialising the file
 with open('model.pickle', 'wb') as handle:
